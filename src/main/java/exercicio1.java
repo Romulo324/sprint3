a916016bd26573ea2f61b684bf4d5d082bd55d79
romulooaskes <romulocesaroaskes@gmail.com>
2023-03-23 20:58:09 -0300

    não sei se tá bem explicado e usei algumas girias que uso no cotidiano mas tenho total conciencia que não devo falar assim em reuniões e clientes

---
+++ src/main/java/exercicio1.java
@@ -0,0 +1,4 @@
+public class exercicio1 {
+//hierarquia criada no sistema fazem com que os  vínculos não sejam estabelecidos entre as tabelas em um modelo relacional,
+// só que entre entidades e seus relacionamentos. tipo, a entidade pai vinculada a uma entidade filha
+}
